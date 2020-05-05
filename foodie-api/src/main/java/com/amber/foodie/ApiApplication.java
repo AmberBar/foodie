abfdb9d5f415cec09e61d4fe3b1ac2bef5c45120
amberlei <amberbar@163.com>
2020-05-06 00:40:55 +0800

    添加flyway数据版本控制

--- foodie-api/src/main/java/com/amber/foodie/ApiApplication.java
+++ foodie-api/src/main/java/com/amber/foodie/ApiApplication.java
@@ -1,14 +1,18 @@
 package com.amber.foodie;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 import tk.mybatis.spring.annotation.MapperScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = FlywayAutoConfiguration.class)
 // 扫描mapper
 @MapperScan(basePackages = "com.amber.foodie.mapper")
+@Slf4j
 public class ApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
+        log.debug("================================");
     }
 }
